c3224f84f629e24da77658ae6d4fb21b875b0b8f
My Name <my-name@chromium.org>
2020-12-12 03:47:43 +0300

    first_live_site

--- postgresTest/testdb/urls.py
+++ postgresTest/testdb/urls.py
@@ -3,5 +3,5 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns = [
-    path('/', views.showTeachers, name='teachers'),
+    path('', views.showTeachers, name='teachers'),
 ]
